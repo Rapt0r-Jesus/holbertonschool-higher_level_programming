913759bafa0fea62d5e9566c422af76647f9b7d6
Rapt0r-Jesus <dorian.oufer@gmail.com>
2025-09-08 14:28:16 +0200

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        index = 0
+        for val in row:
+            if index != 0:
+                print(" ", end="")
+            print("{:d}".format(val), end="")
+            index += 1
+        print()
