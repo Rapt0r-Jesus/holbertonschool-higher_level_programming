09b450a91ad8caee4bccd9ab81bd921d854dcff3
Rapt0r-Jesus <dorian.oufer@gmail.com>
2025-09-16 17:53:52 +0200

    python classes task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class Square:
+    """A square is a geometrical form"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
+    
+    def my_print(self):
+        """Print the square with the character # to stdout."""
+        if self.__size == 0:
+            print()  # Empty line if size is 0
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
