57a4db1fc284d2ac5b6557765ba3d3862fb0a99d
Rapt0r-Jesus <dorian.oufer@gmail.com>
2025-09-19 11:25:41 +0200

    correction pycode task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -25,7 +25,7 @@ class Square:
     def area(self):
         """Return the current square area."""
         return self.__size ** 2
-    
+
     def my_print(self):
         """Print the square with the character # to stdout."""
         if self.__size == 0:
