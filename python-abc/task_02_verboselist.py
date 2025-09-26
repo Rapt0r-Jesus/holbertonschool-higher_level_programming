486386af1d722334a50bce87ba601f8778537d16
Rapt0r-Jesus <dorian.oufer@gmail.com>
2025-09-26 02:45:38 +0200

    task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+class VerboseList(list):
+    """Custom list class that prints notifications on modifications."""
+
+    def append(self, item):
+        """Add an item and notify."""
+        super().append(item)
+        print(f"Added {item} to the list.")
+
+    def extend(self, iterable):
+        """Extend list and notify."""
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with {count} items.")
+
+    def remove(self, item):
+        """Remove item and notify."""
+        print(f"Removed {item} from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        """Pop item and notify."""
+        item = super().pop(index)
+        print(f"Popped {item} from the list.")
+        return item
