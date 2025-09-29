4b40c22b9b22dcc61209e62a842103919f63190e
Rapt0r-Jesus <dorian.oufer@gmail.com>
2025-09-29 20:08:04 +0200

    task 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Append a string at the end of a UTF8 text file
+ and return number of characters added"""
+
+ 
+def append_write(filename="", text=""):
+    """Append a string at the end of a UTF8 text file and return
+    number of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
