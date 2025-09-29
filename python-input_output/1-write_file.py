92515849e82efc5b2b9aee940b2ba59c7d5fee63
Rapt0r-Jesus <dorian.oufer@gmail.com>
2025-09-29 20:01:14 +0200

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Write a string to a UTF8 text file and return number of characters written"""
+"""Write a string to a UTF8 text file and return
+number of characters written"""
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file and return number of characters written"""
+    """Write a string to a UTF8 text file and return
+    number of characters written"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
