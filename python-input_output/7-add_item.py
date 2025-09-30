7dff3618874bfa3b9c7b700d48e1d34b3a31c6d3
Rapt0r-Jesus <dorian.oufer@gmail.com>
2025-09-30 16:24:09 +0200

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""module that add all argument to a Python list
+and then save them to a file"""
+
+
 import sys
 import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
